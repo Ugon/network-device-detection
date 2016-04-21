cd50e965634c12f5aaa901d6d7bb1e34f4009ed7
Szymon Tracz <synom726@gmail.com>
2016-04-21 18:14:17 +0200

    initialized server state

--- server.py
+++ server.py
@@ -1,25 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+macs = [123, 234, 345]
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+  #return "ddd"
+  #return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+  return render_template('index.html', macs=macs)
 
 @app.route('/devices/{mac}', methods=['POST'])
-def add_device():
-	pass
+def add_device(mac):
+	macs.append(mac)
+	return ""
 
 @app.route('/devices/{mac}', methods=['DELETE'])
-def delete_device():
-	pass
+def delete_device(mac):
+	macs.remove(mac)
+	return ""
 
 @app.route('/devices/registered', methods=['GET'])
 def get_registered():
-	pass
+	return "ddd"
 
 @app.route('/devices/connected', methods=['GET'])
 def get_connected():
-	pass
+	return "aaa"
 
 
 if __name__ == "__main__":
